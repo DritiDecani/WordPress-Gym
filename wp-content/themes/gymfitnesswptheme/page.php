3ff0dc8293ef29d0f734155ebd0ff3a0d430f9f4
shpat Radoniqi <95884286+coyg2000@users.noreply.github.com>
2022-06-15 14:36:13 +0200

    Update page.php

--- wp-content/themes/gymfitnesswptheme/page.php
+++ wp-content/themes/gymfitnesswptheme/page.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
-  <main class="container page section">
+  <main class="container page section no-sidebars">
   <?php while(have_posts()): the_post(); ?>
         <h1 class="text-center text-primary"><?php the_title(); ?></h1>
     <?php
         if(has_post_thumbnail()):
-           the_post_thumbnail('blog');
+           the_post_thumbnail('blog',array('class'=>'fetured-image'));
         else:
         echo "<p>No Image</p>";
         endif;
         
     ?>
-        <div class="text-center">
+        
         <?php the_content(); ?>
-        </div>
+        
        
     <?php endwhile; ?>
 
