d29087504f18cb9364d337e8302cde83dd33a3f1
shpat Radoniqi <95884286+coyg2000@users.noreply.github.com>
2022-06-15 18:47:53 +0200

    Update page-with-sidebars.php

--- wp-content/themes/gymfitnesswptheme/page-with-sidebars.php
+++ wp-content/themes/gymfitnesswptheme/page-with-sidebars.php
@@ -6,21 +6,9 @@ Template Name:Page with sidebar
  get_header(); ?>
   <main class="container page section with-sidebar">
      <div class="page-content">
-     <?php while(have_posts()): the_post(); ?>
-        <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-    <?php
-        if(has_post_thumbnail()):
-           the_post_thumbnail('blog',array('class'=>'fetured-image'));
-        endif;
-        
-    ?>
-        
-        <?php the_content(); ?>
-        
-       
-    <?php endwhile; ?>
+        <?php get_template_part('template-parts/page','loop'); ?>
      </div>
-    <?php get_sidebar(); ?>
+  <?php get_sidebar(); ?>
     </main>
    
 
