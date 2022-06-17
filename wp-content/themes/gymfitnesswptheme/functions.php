ffbe3d0473c24bd898b8e505d53c302524973344
DritiDecani <ddecani@gmail.com>
2022-06-17 18:17:07 +0200

    Full fix

--- wp-content/themes/gymfitnesswptheme/functions.php
+++ wp-content/themes/gymfitnesswptheme/functions.php
@@ -33,15 +33,32 @@ function gymfitness_script() {
     wp_enqueue_script('scripts', get_template_directory_uri() . "/js/scripts.js", array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'gymfitness_script');
+
 //Enable feaute images and other stuff
 function gymfitness_setup(){
-   //Register ne image size
+    //Register ne image size
     add_image_size('square',350,350,true);
     add_image_size('portrait',350,724,true);
     add_image_size('box',400,375,true);
     add_image_size('mediumSize',700,400,true);
     add_image_size('blog',966,644,true);
+
+
     //Add featured image
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme','gymfitness_setup'); //when the theme is activated and ready
+
+//Crates a widget zone
+function gymfitness_widgets(){
+    register_sidebar(array(
+        'name'=>'Sidebar',
+        'id'=>'sidebar',
+        'before_widget'=>'<div class="widget">',
+        'after_widget'=>'</div>',
+        'before_title'=>'<h3>',
+        'after_title'=>'</h3>',
+
+    ));
+}
+add_action('widgets_init','gymfitness_widgets');
