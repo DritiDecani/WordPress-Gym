2383ec2ced67975e008a7b8b15a38ddd03fdb86c
DritiDecani <ddecani@gmail.com>
2022-06-18 12:50:40 +0200

    Single page template

--- wp-content/themes/gymfitnesswptheme/template-parts/page-loop.php
+++ wp-content/themes/gymfitnesswptheme/template-parts/page-loop.php
@@ -1,13 +1,23 @@
-<?php while(have_posts()): the_post(); ?>
-        <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+<?php while (have_posts()) : the_post(); ?>
+    <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+    
     <?php
-        if(has_post_thumbnail()):
-           the_post_thumbnail('blog',array('class'=>'fetured-image'));
-        endif;
-        
+   //Check if an image exists
+    if (has_post_thumbnail()) :
+        the_post_thumbnail('blog', array('class' => 'fetured-image'));
+    endif;
+
+
+    //Check the current post type
+    if(get_post_type() === 'gymfitness_classes'):
+        $start_time = get_field('start_time');
+        $end_time = get_field('end_time');
     ?>
-        
-        <?php the_content(); ?>
-        
-       
-    <?php endwhile; ?>
+        <p class="content-class">
+            <?php echo the_field('class_days'). ' - ' .  $start_time ." to ". 
+            $end_time ?>
+        </p> 
+    <?php endif; ?>
+
+    <?php the_content(); ?>
+<?php endwhile; ?>
