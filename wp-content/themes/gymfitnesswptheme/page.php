ad8abdd3adf1ebb470f9876b20b34419cf88a441
coyg2000 <sr47050@ubt-uni.net>
2022-06-22 23:34:39 +0200

    Implementimi i  Contact us dhe Mapit tek po ajo faqe

--- wp-content/themes/gymfitnesswptheme/page.php
+++ wp-content/themes/gymfitnesswptheme/page.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
   <main class="container page section no-sidebars">
+ 
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2449.380317925922!2d20.285834130813782!3d42.662881576493845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1352fd83af919d6b%3A0xb2d49203d787d830!2sMeli%20Fitness!5e0!3m2!1sen!2s!4v1655836702536!5m2!1sen!2s" width="600"
+     height="450" style="border:0;"
+      allowfullscreen="" loading="lazy"
+     referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+
 
   <?php get_template_part('template-parts/page','loop'); ?>
   </main>
