1ef0cfb3ba432dc4f9b7338f87b8b0b73be5dcdd
DritiDecani <ddecani@gmail.com>
2022-06-25 10:24:24 +0200

    Call classes on main-page and add button

--- wp-content/themes/gymfitnesswptheme/front-page.php
+++ wp-content/themes/gymfitnesswptheme/front-page.php
@@ -10,38 +10,54 @@
         <ul class="areas-container">
             <li class="area">
                 <?php
-                    $area1 = get_field('area_1');
-                    $image = wp_get_attachment_image_src($area1['area_image'], 'mediumSize')[0];
+                $area1 = get_field('area_1');
+                $image = wp_get_attachment_image_src($area1['area_image'], 'mediumSize')[0];
                 ?>
                 <img src="<?php echo $image; ?>" />
                 <p><?php echo $area1['area_name']; ?></p>
             </li>
             <li class="area">
                 <?php
-                    $area2 = get_field('area_2');
-                    $image = wp_get_attachment_image_src($area2['area_image'], 'mediumSize')[0];
+                $area2 = get_field('area_2');
+                $image = wp_get_attachment_image_src($area2['area_image'], 'mediumSize')[0];
                 ?>
                 <img src="<?php echo $image; ?>" />
                 <p><?php echo $area2['area_name']; ?></p>
             </li>
             <li class="area">
                 <?php
-                    $area3 = get_field('area_3');
-                    $image = wp_get_attachment_image_src($area3['area_image'], 'mediumSize')[0];
+                $area3 = get_field('area_3');
+                $image = wp_get_attachment_image_src($area3['area_image'], 'mediumSize')[0];
                 ?>
                 <img src="<?php echo $image; ?>" />
                 <p><?php echo $area3['area_name']; ?></p>
             </li>
             <li class="area">
                 <?php
-                    $area4 = get_field('area_4');
-                    $image = wp_get_attachment_image_src($area4['area_image'], 'mediumSize')[0];
+                $area4 = get_field('area_4');
+                $image = wp_get_attachment_image_src($area4['area_image'], 'mediumSize')[0];
                 ?>
                 <img src="<?php echo $image; ?>" />
                 <p><?php echo $area4['area_name']; ?></p>
             </li>
         </ul>
     </section>
+
+    <section class="classes-homepage">
+        <div class="container section">
+            <h2 class="text-primary text-center">Our Classes</h2>
+
+            <?php gymfitness_classes_list(4); ?>
+
+            <div class="button-container">
+                <a class="button" href="<?php echo get_permalink(get_page_by_title('Classes')); ?>">View All Classes</a>
+            </div>
+        </div>
+
+
+    </section>
+
+
 
 <?php endwhile ?>
 
--- wp-content/themes/gymfitnesswptheme/inc/queries.php
+++ wp-content/themes/gymfitnesswptheme/inc/queries.php
@@ -1,11 +1,12 @@
 <?php
 
 
-function gymfitness_classes_list(){ ?>
+function gymfitness_classes_list( $number = -1 ){ ?>
     <ul class="classes-list">
         <?php
             $args = array(
                 'post_type' => 'gymfitness_classes',
+                'posts_per_page' => $number
             );
             //Use WP_Query and append the results into $class
             $classes = new WP_Query($args);
